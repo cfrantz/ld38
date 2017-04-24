4570ec95a1a12337a33a69d9c2aeec1b0f13e1c7
Chris Frantz <frantzcj@gmail.com>
2017-04-23 21:21:13 -0700

    Remove commented code

--- main.cc
+++ main.cc
@@ -3,11 +3,8 @@
 #include <gflags/gflags.h>
 
 #include "app.h"
-//#include "proto/config.pb.h"
 #include "util/config.h"
 
-DEFINE_string(config, "", "config file");
-DEFINE_bool(dump_config, false, "Dump config to stdout and exit");
 
 const char kUsage[] =
 R"ZZZ(<optional flags>
@@ -24,27 +21,8 @@ int main(int argc, char *argv[]) {
     gflags::SetUsageMessage(kUsage);
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-
-    /*
-    auto* config = ConfigLoader<fdgconfig::Graph>::Get();
-    if (!FLAGS_config.empty()) {
-        config->Load(FLAGS_config);
-    } else {
-    }
-    if (FLAGS_dump_config) {
-        puts(config->config().DebugString().c_str());
-        exit(0);
-    }
-    */
-
     ld38::VolcanoApp app("Angry Volcano God");
     app.Init();
-
-    /*
-    if (argc > 1) {
-        app.Load(argv[1]);
-    }
-    */
     app.Run();
     return 0;
 }
